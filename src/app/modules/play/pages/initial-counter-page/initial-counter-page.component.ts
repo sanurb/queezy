493630f2745cbbf16c6a775c9db6079e5a76ba54
SantiagoUrbano <david.urbano@uao.edu.co>
2023-01-08 16:26:54 -0500

    feat: :sparkles: Counter Initial added

--- src/app/modules/play/pages/initial-counter-page/initial-counter-page.component.ts
+++ src/app/modules/play/pages/initial-counter-page/initial-counter-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-initial-counter-page',
   templateUrl: './initial-counter-page.component.html',
-  styleUrls: ['./initial-counter-page.component.scss']
+  styleUrls: ['./initial-counter-page.component.scss'],
 })
-export class InitialCounterPageComponent {
+export class InitialCounterPageComponent implements OnInit, OnDestroy {
+  counter = 3;
+  setInterval: any;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.playcounterInicial();
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.setInterval);
+  }
+
+  playcounterInicial() {
+    this.setInterval = setInterval(() => {
+      console.log('hola');
+      if (this.counter === 0) {
+        this.router.navigate(['/play/takeQuiz']);
+      }
+
+      this.counter = this.counter - 1;
+    }, 1000);
+  }
 }
